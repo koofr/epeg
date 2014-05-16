0d733248d217c2f9bf6a04b38c3c079fbd6b5d1d
Luka Zakrajsek <luka@bancek.net>
2014-05-16 13:32:20 +0200

    Astyle

--- epeg_private.h
+++ epeg_private.h
@@ -17,57 +17,55 @@
 
 typedef struct _epeg_error_mgr *emptr;
 
-struct _epeg_error_mgr
-{
-      struct     jpeg_error_mgr pub;
-      jmp_buf    setjmp_buffer;
+struct _epeg_error_mgr {
+    struct     jpeg_error_mgr pub;
+    jmp_buf    setjmp_buffer;
 };
 
-struct _Epeg_Image
-{
-   struct _epeg_error_mgr          jerr;
-   struct stat                     stat_info;
-   unsigned char                  *pixels;
-   unsigned char                 **lines;
-   
-   char                            scaled : 1;
-   
-   int                             error;
-   
-   Epeg_Colorspace                 color_space;
-   
-   struct {
-      char                          *file;
-      struct {
-	 unsigned char           **data;
-	 int                       size;
-      } mem;
-      int                            w, h;
-      char                          *comment;
-      FILE                          *f;
-      J_COLOR_SPACE                  color_space;
-      struct jpeg_decompress_struct  jinfo;
-      struct {
-	 char                       *uri;
-	 unsigned long long int      mtime;
-	 int                         w, h;
-	 char                       *mime;
-      } thumb_info;
-   } in;
-   struct {
-      char                        *file;
-      struct {
-	 unsigned char           **data;
-	 int                      *size;
-      } mem;
-      int                          x, y;
-      int                          w, h;
-      char                        *comment;
-      FILE                        *f;
-      struct jpeg_compress_struct  jinfo;
-      int                          quality;
-      char                         thumbnail_info : 1;
-   } out;
+struct _Epeg_Image {
+    struct _epeg_error_mgr          jerr;
+    struct stat                     stat_info;
+    unsigned char                  *pixels;
+    unsigned char                 **lines;
+
+    char                            scaled : 1;
+
+    int                             error;
+
+    Epeg_Colorspace                 color_space;
+
+    struct {
+        char                          *file;
+        struct {
+            unsigned char           **data;
+            int                       size;
+        } mem;
+        int                            w, h;
+        char                          *comment;
+        FILE                          *f;
+        J_COLOR_SPACE                  color_space;
+        struct jpeg_decompress_struct  jinfo;
+        struct {
+            char                       *uri;
+            unsigned long long int      mtime;
+            int                         w, h;
+            char                       *mime;
+        } thumb_info;
+    } in;
+    struct {
+        char                        *file;
+        struct {
+            unsigned char           **data;
+            int                      *size;
+        } mem;
+        int                          x, y;
+        int                          w, h;
+        char                        *comment;
+        FILE                        *f;
+        struct jpeg_compress_struct  jinfo;
+        int                          quality;
+        char                         thumbnail_info : 1;
+    } out;
 };
 
 METHODDEF(void) _jpeg_decompress_error_exit(j_common_ptr cinfo);
@@ -79,8 +77,8 @@ METHODDEF(void) _jpeg_term_source(j_decompress_ptr cinfo);
 METHODDEF(void) _jpeg_init_destination(j_compress_ptr cinfo);
 METHODDEF(boolean) _jpeg_empty_output_buffer (j_compress_ptr cinfo);
 METHODDEF(void) _jpeg_term_destination (j_compress_ptr cinfo);
-    
+
 METHODDEF(void) _emit_message (j_common_ptr cinfo, int msg_level);
 METHODDEF(void) _output_message (j_common_ptr cinfo);
-METHODDEF(void) _format_message (j_common_ptr cinfo, char * buffer);
+METHODDEF(void) _format_message (j_common_ptr cinfo, char *buffer);
 #endif
